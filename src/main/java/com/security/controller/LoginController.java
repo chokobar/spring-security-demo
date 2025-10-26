0675d3b8e97863d9f03ebf8a805a733639626ae3
chokobar89 <chokobar89@naver.com>
2025-10-26 09:13:17 +0900

    로그인 화면 추가

---
+++ src/main/java/com/security/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.security.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/auth")
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+
+}
