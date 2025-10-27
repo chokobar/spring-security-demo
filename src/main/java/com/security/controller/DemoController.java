baca427b05bc798368143f606c34ef0adb28ea58
chokobar89 <chokobar89@naver.com>
2025-10-27 19:56:24 +0900

    컨트롤러 수정

--- src/main/java/com/security/controller/DemoController.java
+++ src/main/java/com/security/controller/DemoController.java
@@ -4,16 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-public class DemoController {
-
-    @GetMapping({"/", "/home"})
-    public String home() {
-        return "home";
-    }
-
-    @GetMapping("/admin")
-    public String admin() {
-        return "admin";
-    }
-}
+//@Controller
+//public class DemoController {
+//
+//    @GetMapping({"/", "/home"})
+//    public String home() {
+//        return "home";
+//    }
+//
+//    @GetMapping("/admin")
+//    public String admin() {
+//        return "admin";
+//    }
+//}
