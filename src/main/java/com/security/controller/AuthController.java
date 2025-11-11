f3a69162ef1815f4dfc338eae03521722c144819
chokobar89 <chokobar89@naver.com>
2025-11-11 21:22:30 +0900

    @PreAuthorize 적용

--- src/main/java/com/security/controller/AuthController.java
+++ src/main/java/com/security/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.security.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @Controller
 public class AuthController {
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping({"/", "/home"})
     public String home() {
         return "home";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/admin")
     public String admin() {
         return "admin";
